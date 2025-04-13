159f28102b5a036d88e5617151d628e7a2e3214d
Gvancacuna <gvancabegiashvili@gmail.com>
2025-04-13 12:57:27 +0400

    round reset

--- script.js
+++ script.js
@@ -73,6 +73,8 @@ reset.addEventListener("click", (event) => {
 	resetDice(circles2);
 	gameHistory.replaceChildren();
 	// reseting scores
+	round=0;
+	
 	score1=0;
 	score2=0
 	player1_score.textContent = score1;
